66503c1dd77ac742c73b40cdcde39a7e531fae52
MichaelZVOR <158210406+MichaelZVOR@users.noreply.github.com>
2024-10-27 11:24:13 +0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: "MARC-ANTOINE BARROIS Ganymede",
+        productNumber: '85332',
+        paymentStatus: 'Не оплачен',
+        shipping: 'Ожидается'
+    },
+    {
+        productName: "TIZIANA TERENZI Kirke",
+        productNumber: '84332',
+        paymentStatus: 'Возвращен',
+        shipping: 'Отклонен'
+    },
+    {
+        productName: "VERTUS Narcos'is",
+        productNumber: '81532',
+        paymentStatus: 'Не оплачен',
+        shipping: 'Ожидается'
+    },
+    {
+        productName: "INITIO PARFUMS PRIVES Oud For Greatness",
+        productNumber: '56332',
+        paymentStatus: 'Не оплачен',
+        shipping: 'Ожидается'
+    },
+    {
+        productName: "JO MALONE Wood Sage & Sea Salt",
+        productNumber: '34332',
+        paymentStatus: 'Возвращен',
+        shipping: 'Отклонен'
+    },
+    {
+        productName: "MONTALE Roses Musk",
+        productNumber: '11332',
+        paymentStatus: 'Не оплачен',
+        shipping: 'Ожидается'
+    },
+]
